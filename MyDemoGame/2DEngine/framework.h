03e9f54ddfc14132d0c18a3122d2fb6238fed44e
sanao2 <SanaoAiza1102@gmail.com>
2025-06-04 17:29:27 +0900

    Add files via upload

---
+++ MyDemoGame/2DEngine/framework.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+#include <windows.h>
+#include <assert.h>
+#include <wrl.h>  // ComPtr 사용을 위한 헤더
+
+#include <d3d11.h>
+#pragma comment(lib, "d3d11.lib")
+
+#include <d2d1_3.h> //ID2D1Factory8,ID2D1DeviceContext7
+#pragma comment(lib, "d2d1.lib")
+
+#include <dxgi1_6.h> // IDXGIFactory7
+#pragma comment(lib, "dxgi.lib")
+
+#include <wincodec.h>
+#pragma comment(lib,"windowscodecs.lib")
+
+using namespace Microsoft::WRL;
