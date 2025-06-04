1ac9aab7479919a5f73a0ab6a29b82b904758ac6
Sanao2 <SanaoAiza1102@gmail.com>
2025-06-04 20:20:22 +0900

    .

---
+++ MyDemoGame/2DEngine/RenderManager.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "pch.h"
+
+class RenderManager
+{
+public : 
+	void Initialize(); 
+	void Render(); 
+};
+
--- MyDemoGame/MyDemoGame/MyDemoGame.h
+++ MyDemoGame/MyDemoGame/MyDemoGame.h
@@ -1,3 +1,23 @@
 ﻿#pragma once
-
+#include "../2DEngine/pch.h" 
 #include "resource.h"
+
+class Application
+{
+private : 
+	HWND g_hwnd = nullptr;
+
+protected : 
+	ComPtr<ID3D11Device> g_d3dDevice;
+	ComPtr<IDXGISwapChain1> g_dxgiSwapChain;
+	ComPtr<ID2D1DeviceContext7> g_d2dDeviceContext;
+	ComPtr<ID2D1Bitmap1> g_d2dBitmapTarget;
+
+public : 
+	void Initialize(HWND hwnd); 
+	void Uninitialize();
+	void Render();
+
+	HRESULT CreateBitmapFromFile(const wchar_t* path, ID2D1Bitmap1** outBitmap);
+	int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow);
+};
